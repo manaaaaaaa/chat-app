b44207984a931b18c5505fb6e0f65e9074822825
manaaaaaaaaaa <papiyon135@gmail,com>
2020-08-02 22:18:47 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
+<<<<<<< Updated upstream
   resources :users, only: [:edit, :update]
+=======
+  resources :users
+>>>>>>> Stashed changes
   resources :rooms, only: [:new, :create]
 end
